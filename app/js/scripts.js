2f647f79a7ebcc210288454f7d39263e6b5d203b
mozolevsky <alf.rulit@gmail.com>
2017-03-27 19:14:36 +0300

    Made fitness page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -44,14 +44,61 @@ function makeTabs(controlBlock, containerBlock, activeTabControl, activeArea) {
     }
 };
 
-    /* tabs nutrition meal */
-    makeTabs('.meals-tabs', '.meals-tabs__container', 'meals-tabs__link_active', 'meals-tabs__tab-area_active');
+/* tabs nutrition meal */
+makeTabs('.meals-tabs', '.meals-tabs__container', 'meals-tabs__link_active', 'meals-tabs__tab-area_active');
 
-    /* transform tabs */
-    makeTabs('.transform__block', '.transform__tabs-container', 'transform__letter_active', 'transform__tab-container_active');
-
-
+/* transform tabs */
+makeTabs('.transform__block', '.transform__tabs-container', 'transform__letter_active', 'transform__tab-container_active');
 
 
+/* the movement of the arrows function in accordions on the fitness page */
+function moveArrow(mainArea, arrowParentNode, arrowClass, turnedClass) {
+    if (mainArea) {
+        mainArea.addEventListener('click', function(e) {
+            var target = e.target;
 
+            while (target.className !== mainArea.className) {
 
+                if (target.className.indexOf(arrowParentNode) !== -1) {
+                    var arrow = target.querySelector('.' + arrowClass);
+                    if (arrow.className.indexOf(turnedClass) == -1) {
+                        arrow.classList.add(turnedClass);
+                    } else {
+                        arrow.classList.remove(turnedClass);
+                    }
+                    break;
+                }
+                target = target.parentNode;
+            }
+        });
+    }
+}
+
+/* moving arrows in accordions */
+var fitness = document.querySelector('.fitness');
+moveArrow(fitness, 'panel-heading', 'fitness__accordeon-arrow', 'arrow-turned');
+
+var accordion2 = document.getElementById('accordion2');
+moveArrow(accordion2, 'accordion-inner__header', 'accordion-inner__arrow', 'accordion-inner__arrow_turned');
+
+var accordion3 = document.getElementById('accordion2');
+moveArrow(accordion3, 'accordion-inner__header', 'accordion-inner__arrow', 'accordion-inner__arrow_turned');
+
+var accordion4 = document.getElementById('accordion2');
+moveArrow(accordion4, 'accordion-inner__header', 'accordion-inner__arrow', 'accordion-inner__arrow_turned');
+
+/* Manage containers in accordions */
+$(window).ready(function(){
+    if ($(window).width() >= 768){
+        $('.accordion-inner__body-wrapper').addClass('in');
+    }
+    if ($(window).width() <= 768){
+        $('.accordion-inner__body-wrapper').removeClass('in');
+    }
+});
+
+$('.accordion-inner__header[data-toggle="collapse"]').click(function(e){
+    if ($(window).width() >= 768) {
+        e.stopPropagation();
+    }
+});
