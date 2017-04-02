49e6a7939e2b5032757b16d60a76ff56481dc09b
DariaYarohovich <dashayarohovich@mail.ru>
2017-04-02 17:41:12 +0300

    Popups, ajax

--- app/js/addMeasure.js
+++ app/js/addMeasure.js
@@ -14,3 +14,19 @@ var addButtonHandler = function (elem) {
 var visibilityHandler = function(elem) {
     elem.classList.toggle('invisible');
 };
+
+$(document).ready(function(){
+    $(".tracking__add_filled").change(function postinput(){
+        var trackingValue = $(this).val(); // this.value
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:3000/test.php',
+            data: { trackingValue: trackingValue }
+        }).done(function(responseData) {
+            console.log('Done: ', responseData);
+        }).fail(function() {
+            console.log('Failed');
+        });
+    });
+});
+
