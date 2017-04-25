125af1d0f71bc7ce5d570ca3d7f4479a365039fd
DariaYarohovich <dashayarohovich@mail.ru>
2017-04-25 18:59:37 +0300

    Edited inputs

--- app/js/addMeasure.js
+++ app/js/addMeasure.js
@@ -6,21 +6,54 @@ for (var i = 0; i < buttons.length; i++) {
 
     buttons[i].addEventListener('click', function (event) {
         addButtonHandler(event.target);
+
         var elem;
 
-        if (event.target.nextElementSibling.childNodes[0]) {
-            elem  = event.target.nextElementSibling.childNodes[0];
+        if (event.target.nextElementSibling.childNodes[1].childNodes[1]) {
+            elem = event.target.nextElementSibling.childNodes[1].childNodes[1];
         } else {
-            elem = event.target.nextElementSibling;
+            elem = event.target.nextElementSibling.childNodes[1].childNodes[0];
         }
 
         elem.focus();
-        elem.onblur = function () {
-            if (elem.value) {
-                elem.classList.add('tracking__add_filled-text');
+
+        elem.onkeyup = function (){
+
+            if (!isNaN(elem.value)) {
+                elem.classList.remove('tracking__add_filled-invalid');
+                elem.classList.add('tracking__add_filled-valid');
             } else {
+                elem.classList.remove('tracking__add_filled-valid');
+                elem.classList.add('tracking__add_filled-invalid');
+            }
+        };
+
+        elem.onblur = function () {
+            if (elem.value && elem.classList.contains('tracking__add_filled-valid')) {
+                elem.classList.remove('tracking__add_filled-invalid');
+                elem.classList.remove('tracking__add_filled-valid');
+                elem.classList.add('tracking__add_filled-text');
+
+                $(document).ready(function(){
+                    $(".tracking__add_filled").change(function postinput(){
+                        var trackingValue = $(this).val(); // this.value
+                        $.ajax({
+                            type: 'post',
+                            url: 'http://localhost:3000/test.php',
+                            data: { trackingValue: trackingValue }
+                        }).done(function(responseData) {
+                            console.log('Done: ', responseData);
+                        }).fail(function() {
+                            console.log('Failed');
+                        });
+                    });
+                });
+            } else {
+                elem.classList.remove('tracking__add_filled-invalid');
+                elem.classList.remove('tracking__add_filled-valid');
                 elem.classList.remove('tracking__add_filled-text');
                 addButtonHandler(event.target);
+                elem.value = '';
             }
         }
     });
@@ -35,25 +68,12 @@ for (var i = 0; i < buttons.length; i++) {
 
 var addButtonHandler = function (elem) {
     visibilityHandler(elem);
-    visibilityHandler(elem.nextElementSibling);
+    visibilityHandler(elem.nextElementSibling.childNodes[1]);
 };
 
 var visibilityHandler = function(elem) {
     elem.classList.toggle('invisible');
 };
 
-$(document).ready(function(){
-    $(".tracking__add_filled").change(function postinput(){
-        var trackingValue = $(this).val(); // this.value
-        $.ajax({
-            type: 'post',
-            url: 'http://localhost:3000/test.php',
-            data: { trackingValue: trackingValue }
-        }).done(function(responseData) {
-            console.log('Done: ', responseData);
-        }).fail(function() {
-            console.log('Failed');
-        });
-    });
-});
+
 
